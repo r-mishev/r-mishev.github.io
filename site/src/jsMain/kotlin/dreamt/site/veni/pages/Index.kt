5b4935efb6d1f76e4d08e556cfb149525448b058
Radko Mishev <rm.mishev@gmail.com>
2023-05-19 00:02:22 +0300

    Initial commit

---
+++ site/src/jsMain/kotlin/dreamt/site/veni/pages/Index.kt
@@ -0,0 +1,11 @@
+package dreamt.site.veni.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.core.Page
+
+@Page
+@Composable
+fun HomePage() {
+    // TODO: Add content here
+    println("Hello World!")
+}
